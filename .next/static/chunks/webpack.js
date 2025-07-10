5d534fef917a1b4b7ba8fd69b253470c409990ec
Igor Leal <igorleal.bin@gmail.com>
2025-07-09 21:23:06 -0300

    teste

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a83978b7e5fafce5"; }
+/******/ 		__webpack_require__.h = function() { return "fe8cadad415eb460"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.26ce6c657be01632.hot-update.js
+++ .next/static/webpack/webpack.26ce6c657be01632.hot-update.js
@@ -35,7 +35,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "839ac9e19c7306cb"; }
+/******/ 	__webpack_require__.h = function() { return "fe8cadad415eb460"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/make namespace object */
